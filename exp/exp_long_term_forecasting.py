9acb145cc561d53fbbc02ed0f701234d5c44c062
gardiens <pierrick.bournez@gmail.com>
2023-07-25 15:56:17 +0200

    a

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -227,7 +227,7 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                 self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
             except FileNotFoundError:
                 args1=get_args_from_filename(setting)
-                args1.get_cat_value="_"+str(args1.cv)
+                args1.get_cat_value="_"+str(args1.get_cat_value)
                 setting1=get_settings(args1)
                 self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting1, 'checkpoint.pth')))
 
