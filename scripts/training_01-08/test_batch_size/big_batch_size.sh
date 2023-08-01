ad5e0da3390ba90d6038b66b90099c52bc9cd7d1
gardiens <pierrick.bournez@gmail.com>
2023-08-01 15:52:02 +0200

    ajout MulGPU

--- scripts/training_01-08/test_batch_size/big_batch_size.sh
+++ scripts/training_01-08/test_batch_size/big_batch_size.sh
@@ -69,3 +69,63 @@ python -u run.py \
   --use_gpu 1 \
   --train_epochs 5
 
+
+data=NTU
+batch_size=1024
+model_id_name=NTU01-08Auto${model_name}${batch_size}
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path './dataset/NTU_RGB+D/' \
+  --data_path 'numpyed/'\
+  --model_id ${model_id_name} \
+  --model ${model_name} \
+  --data ${data} \
+  --features M \
+  --seq_len 16 \
+  --label_len ${pred_len} \
+  --pred_len ${pred_len} \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 75 \
+  --dec_in 75 \
+  --c_out 75 \
+  --des 'Exp' \
+  --itr 1      \
+  --dropout 0.1 \
+  --embed timeNTU \
+  --get_cat_value 0 \
+  --get_time_value 1 \
+  --use_gpu 1 \
+  --train_epochs 5
+
+data=NTU
+batch_size=2048
+model_id_name=NTU01-08Auto${model_name}${batch_size}
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path './dataset/NTU_RGB+D/' \
+  --data_path 'numpyed/'\
+  --model_id ${model_id_name} \
+  --model ${model_name} \
+  --data ${data} \
+  --features M \
+  --seq_len 16 \
+  --label_len ${pred_len} \
+  --pred_len ${pred_len} \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 75 \
+  --dec_in 75 \
+  --c_out 75 \
+  --des 'Exp' \
+  --itr 1      \
+  --dropout 0.1 \
+  --embed timeNTU \
+  --get_cat_value 0 \
+  --get_time_value 1 \
+  --use_gpu 1 \
+  --train_epochs 5
