cc46dea35e5ccef7fb064818b2e2d034836f1706
gardiens <pierrick.bournez@gmail.com>
2023-08-01 11:45:29 +0200

    oui

--- models/Metaformer.py
+++ models/Metaformer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models import FEDformer
+from models.FEDformer import Model as FEDformer
 from models import Autoformer 
 import copy 
 dict_membre={
