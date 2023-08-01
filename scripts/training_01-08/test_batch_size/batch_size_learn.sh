af7eefd7a3c2c04e9d074afc8cffc39f2eb62c68
gardiens <pierrick.bournez@gmail.com>
2023-08-01 16:15:15 +0200

    jsuis nul

--- scripts/training_01-08/test_batch_size/batch_size_learn.sh
+++ scripts/training_01-08/test_batch_size/batch_size_learn.sh
@@ -32,7 +32,8 @@ python -u run.py \
   --get_cat_value 0 \
   --get_time_value 1 \
   --use_gpu 1 \
-  --train_epochs 5
+  --train_epochs 5\
+  --batch_size ${batch_size}
 
 
 model_name=FEDformer
@@ -65,7 +66,8 @@ python -u run.py \
   --get_cat_value 0 \
   --get_time_value 1 \
   --use_gpu 1 \
-  --train_epochs 5
+  --train_epochs 5\
+  --batch_size ${batch_size}
 
 model_name=FEDformer
 pred_len=32
@@ -97,73 +99,7 @@ python -u run.py \
   --get_cat_value 0 \
   --get_time_value 1 \
   --use_gpu 1 \
-  --train_epochs 5
+  --train_epochs 5\
+  --batch_size ${batch_size}
 
-
-model_name=FEDformer
-pred_len=32
-data=NTU
-batch_size=256
-model_id_name=NTU01-08Auto${model_name}${batch_size}
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path './dataset/NTU_RGB+D/' \
-  --data_path 'numpyed/'\
-  --model_id ${model_id_name} \
-  --model ${model_name} \
-  --data ${data} \
-  --features M \
-  --seq_len 16 \
-  --label_len ${pred_len} \
-  --pred_len ${pred_len} \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 75 \
-  --dec_in 75 \
-  --c_out 75 \
-  --des 'Exp' \
-  --itr 1      \
-  --dropout 0.1 \
-  --embed timeNTU \
-  --get_cat_value 0 \
-  --get_time_value 1 \
-  --use_gpu 1 \
-  --train_epochs 5
-
-
-
-model_name=FEDformer
-pred_len=32
-
-data=NTU
-batch_size=512
-model_id_name=NTU01-08Auto${model_name}${batch_size}
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path './dataset/NTU_RGB+D/' \
-  --data_path 'numpyed/'\
-  --model_id ${model_id_name} \
-  --model ${model_name} \
-  --data ${data} \
-  --features M \
-  --seq_len 16 \
-  --label_len ${pred_len} \
-  --pred_len ${pred_len} \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 75 \
-  --dec_in 75 \
-  --c_out 75 \
-  --des 'Exp' \
-  --itr 1      \
-  --dropout 0.1 \
-  --embed timeNTU \
-  --get_cat_value 0 \
-  --get_time_value 1 \
-  --use_gpu 1 \
-  --train_epochs 5
 
