a9e77ca113cd97d2080d4e8f3479e7a2e14f265a
gardiens <pierrick.bournez@gmail.com>
2023-08-03 09:55:24 +0200

    ajout réel

---
+++ scripts/training_02-08/pred_membre/script_manquant.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+model_name=FEDformer
+pred_len=32
+
+batch_size=256
+
+i=3
+epoch=14
+
+
+data=NTU-arm
+
+model_id_name=NTU02-08${model_name}leg-lr-${i}-bs${batch_size}
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path './dataset/NTU_RGB+D/' \
+  --data_path 'numpyed/'\
+  --model_id ${model_id_name} \
+  --model ${model_name} \
+  --data ${data} \
+  --features M \
+  --seq_len 16 \
+  --label_len ${pred_len} \
+  --pred_len ${pred_len} \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 36 \
+  --dec_in 36 \
+  --c_out 36 \
+  --des 'Exp' \
+  --itr 1      \
+  --dropout 0.1 \
+  --embed timeNTU \
+  --get_cat_value 0 \
+  --get_time_value 1 \
+  --use_gpu 1 \
+  --train_epochs ${epoch} \
+  --batch_size ${batch_size}\
+  --learning_rate $(echo "scale=10; 10^-$i" | bc)\
+  --split_train_test action \
+  --lradj sem_constant \
+  --patience 4 \
+  --no_test
+
