c2c972b3c19a3e281c294d660f67df04975f8436
gardiens <pierrick.bournez@gmail.com>
2023-08-07 16:22:25 +0200

    marhce connard

--- scripts/07-08/data_aug/plus_daug.sh
+++ scripts/07-08/data_aug/plus_daug.sh
@@ -6,28 +6,24 @@ label_len=32
 data=NTU
 batch_size=256
 
-
 moving_value="0.2"
-for i in 1 2 3 4;
-do 
+for i in 1 2 3 4; do
   output=""
-  for ((j=1;j<=4;j++));do
-      if ((j == i)); then output+=",$moving_value" else output+=",0" fi
-        
-    done
-  echo "${output:1}" done 
+  for ((j=1; j<=4; j++)); do
+    if ((j == i)); then
+      output+=",$moving_value"
+    else
+      output+=",0"
+    fi
+  done
+  echo "${output:1}"
 
-  model_name=FEDformer
-  pred_len=32
-  label_len=32
-  data=NTU
-  batch_size=256
   model_id_name=07-08-daug-"${output:1}"
   python -u run.py \
     --task_name long_term_forecast \
     --is_training 1 \
     --root_path './dataset/NTU_RGB+D/' \
-    --data_path 'numpyed/'\
+    --data_path 'numpyed/' \
     --model_id ${model_id_name} \
     --model ${model_name} \
     --data ${data} \
@@ -42,19 +38,18 @@ do
     --dec_in 75 \
     --c_out 75 \
     --des 'Exp' \
-    --itr 1      \
+    --itr 1 \
     --dropout 0.1 \
     --embed timeNTU \
     --get_cat_value 0 \
     --get_time_value 1 \
     --use_gpu 1 \
-    --train_epochs 14\
+    --train_epochs 14 \
     --lradj sem_constant \
     --patience 4 \
-    --batch_size ${batch_size}\
-    --learning_rate $(echo "scale=10; 10^-3" | bc)\
+    --batch_size ${batch_size} \
+    --learning_rate $(echo "scale=10; 10^-3" | bc) \
     --split_train_test action \
     --augment \
     --prop "${output:1}"
-  done 
-
+done
