b36cfb283e31df9c4712ef7b5e7102b39f08cfc6
gardiens <pierrick.bournez@gmail.com>
2023-08-07 16:16:15 +0200

    a

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -174,9 +174,9 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                         outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
 
                     f_dim = -1 if self.args.features == 'MS' else 0
-                    outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                    outputs = outputs[:, -self.args.label_len:, f_dim:]
                     
-                    batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                    batch_y = batch_y[:, -self.args.label_len:, f_dim:].to(self.device)
 
                     loss = criterion(outputs, batch_y)
                     train_loss.append(loss.item())
