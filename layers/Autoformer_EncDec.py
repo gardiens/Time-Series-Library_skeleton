ddc08f7d487c1e8c3a2abe36d9ac8605d935415c
gardiens <pierrick.bournez@gmail.com>
2023-08-10 16:29:29 +0200

    add formers

--- layers/Autoformer_EncDec.py
+++ layers/Autoformer_EncDec.py
@@ -105,7 +105,8 @@ class EncoderLayer(nn.Module):
         y = self.dropout(self.conv2(y).transpose(-1, 1))
         #res, _ = self.decomp2(x + y) #! A REMETTRE
         #! Ce que moi j'aurai fait 
-        res,_=self.decomp2(y)+x 
+        res,_=self.decomp2(y)
+        res=res+x #! C'est _a 
         return res, attn
 
 
@@ -175,7 +176,7 @@ class DecoderLayer(nn.Module):
         y = x
         y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
         y = self.dropout(self.conv2(y).transpose(-1, 1))
-        x, trend3 = x+self.decomp3(y) #! self.decomp3(x+y)
+        x, trend3 = self.decomp3(x+y) #! self.decomp3(x+y)
 
         residual_trend = trend1 +trend3 #! +trend2
         residual_trend = self.projection(residual_trend.permute(0, 2, 1)).transpose(1, 2)
